21826f5894cfb6f3ce61196dc2877cd5936562c5
mahdee18 <mahdeec10@gmail.com>
2023-06-07 19:22:59 +0600

    Initial Project setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<div className="uppercase text-red-400">Hello world!</div>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+    </>
+  )
+}
+
+export default App

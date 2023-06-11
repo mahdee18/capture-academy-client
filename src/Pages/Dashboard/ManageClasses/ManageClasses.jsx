6c1a642671656fe5466258d1ad1b2b81f2c1e322
mahdee18 <mahdeec10@gmail.com>
2023-06-11 23:24:45 +0600

    Admin aprovedsystem implemented

--- src/Pages/Dashboard/ManageClasses/ManageClasses.jsx
+++ src/Pages/Dashboard/ManageClasses/ManageClasses.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 import useData from "../../../Hooks/useData";
+import Swal from "sweetalert2";
 
 const ManageClasses = () => {
-  const [data] = useData();
+  const [data, ,refetch] = useData();
   console.log(data);
+
+  const handlePending = (classInfo) => {
+    console.log(classInfo);
+    fetch(`http://localhost:3000/alldata/${classInfo}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `Class Approved !`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
   return (
     <div className="mb-12">
       <h2 className="text-center text-2xl font-semibold my-12">
@@ -53,9 +74,18 @@ const ManageClasses = () => {
                 </td>
                 <td>Pending</td>
                 <td className="flex flex-col gap-2 text-white">
-                  <button className="btn btn-ghost btn-xs bg-green-600 hover:text-black">
-                    Approve
-                  </button>
+                  {classInfo.class_status === "approved" ? (
+                    <button className=" w-full btn btn-sm btn-disabled font-bold ">
+                      Approved
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => handlePending(classInfo.class_status)}
+                      className="btn btn-ghost btn-xs bg-green-600 hover:text-black"
+                    >
+                      Approve
+                    </button>
+                  )}
                   <button className="btn btn-ghost btn-xs bg-red-600 hover:text-black">
                     Deny
                   </button>

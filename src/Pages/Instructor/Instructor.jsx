fd3ac5a77540b9a662396d3273db524426eaa9e6
mahdee18 <mahdeec10@gmail.com>
2024-01-03 23:01:02 +0600

    Create a section title

--- src/Pages/Instructor/Instructor.jsx
+++ src/Pages/Instructor/Instructor.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import useData from '../../Hooks/useData';
 import InstructorCards from '../Home/PopularInstructor/InstructorCards';
+import SectionTitle from '../../components/SectionTitle';
 
 const Instructor = () => {
     const [data] = useData()
     return (
         <>
-            <div className='text-center text-white my-10 py-10 bg-green-500 '>
-                <h2 className='md:text-5xl text-3xl font-bold'>Meet Our Expert Instructors</h2>
-                <p className='text-lg py-3'> Experience the Art of Photography with Our Passionate Mentors</p>
-            </div>
-            <div className='w-5/6 mx-auto'>
+
+
+            <div className='w-5/6 mx-auto py-20'>
 
                 <InstructorCards></InstructorCards>
             </div>
---
+++ src/components/SectionTitle.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SectionTitle = ({image, title, subtitle }) => {
+    return (
+        <div className='mb-10 '>
+            <h1 className='text-3xl text-white mb-3'></h1>
+            <h4 className='text-[#d28d0d] uppercase'></h4>
+
+            <section className="relative text-center">
+                <div className="h-96 bg-cover bg-center bg-fixed" style={{ backgroundImage: `url(${image})` }}>
+                    <div className="absolute inset-0 bg-black opacity-70"></div>
+                    <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
+                        <h2 className='md:text-5xl text-3xl font-bold'>{title}</h2>
+                        <p className='text-lg py-3'>{subtitle}</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default SectionTitle;

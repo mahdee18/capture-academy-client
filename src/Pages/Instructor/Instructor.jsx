c425aea48abadf7866b763d414d1ae254f325824
mahdee18 <mahdeec10@gmail.com>
2024-01-03 23:01:56 +0600

    Generate a Instructor Section title

--- src/Pages/Instructor/Instructor.jsx
+++ src/Pages/Instructor/Instructor.jsx
@@ -7,7 +7,7 @@ const Instructor = () => {
     const [data] = useData()
     return (
         <>
-
+            <SectionTitle image={"https://th.bing.com/th/id/R.e35baf115b9d9251841372b8768c3594?rik=HJCjZuIzYh58QA&pid=ImgRaw&r=0"} title={"Meet Our Expert Instructors"} subtitle={"Experience the Art of Photography with Our Passionate Mentors"}></SectionTitle>
 
             <div className='w-5/6 mx-auto py-20'>
 

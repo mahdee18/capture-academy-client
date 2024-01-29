e20ba811638fbead74e54c5e3630be81eb93c74d
mahdee18 <mahdeec10@gmail.com>
2024-01-30 00:26:32 +0600

    Added SWAL

--- src/Pages/Home/Newsletter/NewsLetter.jsx
+++ src/Pages/Home/Newsletter/NewsLetter.jsx
@@ -24,7 +24,7 @@ const NewsLetter = () => {
                 if (data.acknowledged == true) {
                     Swal.fire({
                         title: 'Success!',
-                        text: 'Toy Added successfully!!',
+                        text: 'Congrats! You have subscribed our newsletter successfully!!',
                         icon: 'success',
                         confirmButtonText: 'Cool'
                     })
